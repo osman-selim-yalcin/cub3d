6affd430fceaceb54f0df18663f45ef333589606
osman yalçın <osmanyalcin989@gmail.com>
2022-12-21 14:48:12 +0300

    little frame fix

--- print_frame.c
+++ print_frame.c
@@ -33,24 +33,23 @@ void print_frame(t_game *game, int action)
 	}
 	a = SCREEN_WID / 2 - 10;
 	b = SCREEN_LEN / 2 - 10;
+	int x_move = SCREEN_WID / 50;
+	int y_move = - SCREEN_LEN / 10;
 	while (a < SCREEN_WID / 2 + 10)
 	{
 		if (!(a < SCREEN_WID / 2 + 3 && a > SCREEN_WID / 2 - 3))
-		{
-			my_mlx_pixel_put_frame(game, a + 30, SCREEN_LEN / 2 - 80,  create_trgb(0, 200, 100, 100));
-		}
+			my_mlx_pixel_put_frame(game, a + x_move, SCREEN_LEN / 2 + y_move,  create_trgb(0, 200, 100, 100));
 		a++;
 	}
 	while (b < SCREEN_LEN / 2 + 10)
 	{
 		if (!(b < SCREEN_LEN / 2 + 3 && b > SCREEN_LEN / 2 - 3))
-			my_mlx_pixel_put_frame(game, SCREEN_WID / 2 + 30, b - 80,  create_trgb(0, 200, 100, 100));
+			my_mlx_pixel_put_frame(game, SCREEN_WID / 2 + x_move, b + y_move,  create_trgb(0, 200, 100, 100));
 		b++;
 	}
 	
 	
 }
-
 
 void	my_mlx_pixel_put_frame(t_game *game, int x, int y, int color)
 {
